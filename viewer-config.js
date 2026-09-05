a7b5ccfe979ded66055bb8497f850f2fc8cf259a
akarenka <akarenka1111@gmail.com>
2026-09-05 15:33:42 +0800

    Add files via upload

---
+++ viewer-config.js
@@ -0,0 +1,4 @@
+window.STREAMPULSE_GOOGLE_VIEWER_CONFIG = {
+  clientId: '299554087628-qtkrqporl2cget2cofubhdijm167gpi4.apps.googleusercontent.com',
+  viewerApiUrl: 'https://REPLACE_WITH_CLOUD_FUNCTION_URL/viewerApi'
+};
