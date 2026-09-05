4ef33f2469b69e1297c04ad2da9b50f1833cca76
akarenka <akarenka1111@gmail.com>
2026-09-05 07:00:14 -0400

    Delete viewer-config.js

--- viewer-config.js
+++
@@ -1,4 +0,0 @@
-window.STREAMPULSE_GOOGLE_VIEWER_CONFIG = {
-  clientId: '299554087628-qtkrqporl2cget2cofubhdijm167gpi4.apps.googleusercontent.com',
-  viewerApiUrl: 'https://REPLACE_WITH_CLOUD_FUNCTION_URL/viewerApi'
-};
